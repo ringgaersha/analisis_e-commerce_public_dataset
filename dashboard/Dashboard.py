055633fdfc4bdcf3e2df7ef61c98e1c6bfd3d5b5
ringgaersha <ringgaersha200701@gmail.com>
2023-09-29 22:19:34 +0700

    Commit Berhasil

---
+++ dashboard/Dashboard.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from PIL import Image
+import os
+
+st.set_page_config(
+    page_title="E-Commerce Data Analysis",
+    page_icon="📊",
+)
+
+st.title("DASHBOARD E-COMMERCE PUBLIC DATASET")
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="DASHBOARD",
+        options=["About Projects","Dataset Overview"],
+    )
+
+if selected == "About Projects":
+    path = os.path.dirname(__file__)
+    my_file = path+'/images/gambar1.jpg'
+    image = Image.open(my_file)
+    resized_image = image.resize((650, 350))
+    st.image(resized_image, caption='E-Comerce Analysis Dashboard')
+    # col1, col2, col3 = st.columns(3)
+
+    # with col1:
+    st.write("Dashboard ini menggunakan Brazilian E-Commerce Public Dataset sebagai sumber datanya. Dataset ini berisi informasi tentang transaksi e-commerce di Brazil dan memberikan gambaran tentang tren dan pola pembelian online di negara tersebut.")
+
+
+    # with col2:
+    #     st.write("Dalam dashboard ini, dibuat visualisasi data yang menarik. Visualisasi ini akan membantu untuk memahami berbagai aspek penjualan e-commerce di Brazil, termasuk informasi tentang kota dengan jumlah penjual terbanyak, kategori produk yang paling populer, dan tren pembelian selama periode waktu tertentu.")
+
+    
+    # with col3:
+    #     st.write("Selain itu, dashboard ini juga dilengkapi dengan fitur interaktif, yang memungkinkan untuk mengeksplorasi data dengan cara yang lebih dalam.")
+    #     st.write("Tujuan dari projek ini adalah untuk memberikan dashboard yang interaktif dan informatif dalam mempelajari dan menggali informasi dari dataset E-Commerce Public Dataset.")
+
+    
+
+if selected == "Dataset Overview":
+
+    st.write("Dataset yang digunakan adalah E-Commerce Public Dataset.")
+    st.write("Dataset ini mencakup informasi yang dikumpulkan dari transaksi e-commerce di Brazil dalam rentang waktu tertentu. Dataset tersebut terdiri dari beberapa file yang terkait satu sama lain dan memberikan wawasan yang komprehensif tentang industri e-commerce di Brazil.")
+    st.write("Beberapa file utama dalam dataset ini termasuk:")
+    st.write("- Orders: File ini berisi informasi tentang pesanan, seperti ID pesanan, status pesanan, waktu pembelian, waktu persetujuan, dan waktu pengiriman pesanan.")
+    st.write("- Order Items: File ini berisi informasi terperinci tentang setiap item yang dibeli dalam pesanan, seperti ID produk, harga produk, jumlah yang dibeli, dan biaya pengiriman.")
+    st.write("- Products: File ini berisi informasi tentang produk, termasuk ID produk, kategori produk, dan nama produk.")
+    st.write("- Sellers: File ini berisi informasi tentang penjual, seperti ID penjual, nama penjual, dan lokasi penjual (kota dan negara bagian).")
+    st.write("- Customers: File ini berisi informasi tentang pelanggan, seperti ID pelanggan, nama pelanggan, dan lokasi pelanggan (kota dan negara bagian).")
+    st.write("- Geolocation: File ini menyediakan informasi geografis tentang kota dan negara bagian di Brazil. Ini membantu dalam analisis berdasarkan lokasi geografis.")
+    
---
+++ dashboard/pages/order_analysis.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+import os
+from folium.plugins import HeatMap
+from streamlit_folium import folium_static
+from streamlit_option_menu import option_menu
+from babel.numbers import format_currency
+
+st.set_page_config(
+    page_title="Order and Purchase",
+    page_icon="🛒",
+)
+
+path = os.path.dirname(__file__)
+my_file = path+'/../all_data.csv'
+all_df = pd.read_csv(my_file)
+datetime_columns = ["order_approved_at", "order_delivered_customer_date"]
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+def df_delivery_Status(df):
+    df_delivery_Status = df.groupby(by="delivery_status").agg({
+        "order_id": "count"
+    }).sort_values(by="order_id",ascending=False).reset_index()
+    
+    return df_delivery_Status
+
+def df_payment_type(df):
+    df_payment_type = df.groupby(by="payment_type").agg({
+        "customer_id": "count",
+    }).reset_index()
+    
+    return df_payment_type
+
+def df_product_sales(df):
+
+    df_product_sales = all_df.groupby(by="order_purchase_time").agg({
+        "customer_id": "count"
+    }).sort_values(by="customer_id",ascending=False).reset_index()
+    
+    return df_product_sales
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Order and Purchase",
+        options=["Order Delivery Status","Payment Type","Purchase Time"],
+    )
+
+if selected == "Order Delivery Status":
+    st.title("Order Delivery Status")
+
+    df_delivery_Status = df_delivery_Status(all_df)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=df_delivery_Status['delivery_status'],
+        y=df_delivery_Status['order_id'],
+        text=df_delivery_Status['order_id'],  # Menambahkan label di setiap bar
+        textposition='outside',  # Menampilkan label di luar bar
+        textfont=dict(
+            color='white',  # Mengatur warna teks menjadi putih
+        ),
+        marker=dict(
+            color='rgb(173, 216, 230)',  # Set color here
+        ),
+    ))
+
+    fig.update_layout(
+        title='Order Delivery Status Over 2016 - 2018',
+        xaxis=dict(title='Product Name', tickangle=45),
+        yaxis=dict(title='Total Sales'),
+        showlegend=False,
+        width=700,
+        height=500,
+    )
+
+    st.plotly_chart(fig)
+
+    with st.expander("Interpretasi", expanded=False):
+        # Konten expander
+        st.write("- Hampir ebagian besar produk yaitu sejumlah 106893 produk yang terjual berhasil sampai ke pelanggan dengan tepat waktu.")
+        st.write("- Masih ada sebesar 8715 barang yang masih terlambat untuk sampai ke pelanggan.")
+
+
+if selected == "Payment Type":
+    
+    st.title("Customer Payment Type")
+    df_payment_type = df_payment_type(all_df)
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=df_payment_type['payment_type'],
+        y=df_payment_type['customer_id'],
+        text=df_payment_type['customer_id'],  # Menambahkan label di setiap bar
+        textposition='outside',  # Menampilkan label di luar bar
+        textfont=dict(
+            color='white',  # Mengatur warna teks menjadi putih
+        ),
+        marker=dict(
+            color='rgb(173, 216, 230)',  # Set color here
+        ),
+    ))
+
+    fig.update_layout(
+        title='Customer Payment Type Over 2016 - 2018',
+        xaxis=dict(title='Product Name', tickangle=45),
+        yaxis=dict(title='Total Sales'),
+        showlegend=False,
+        width=700,
+        height=500,
+    )
+
+    st.plotly_chart(fig)
+
+    with st.expander("Interpretasi", expanded=False):
+
+            # Konten expander
+            st.write("- Jenis pembayaran yang paling banyak digunakan pelanggan adalah credit card dan juga boleto dengan jumlah transaksi sebanyak 85277 dan 22510 transaksi pada masing - masing jenis pembayaran tersbut.")
+            st.write("- Jenis pembayaran yang paling sedikit digunakan pelanggan adalah debit card dan voucher yaitu hanya sebesar 1659 dan 6162 transaksi pada masing - masing jenis pembayaran tersbut.")
+    
+
+if selected == "Purchase Time":
+    st.title("Customer Purchase Time")
+    df_product_sales = df_product_sales(all_df)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=df_product_sales['order_purchase_time'],
+        y=df_product_sales['customer_id'],
+        text=df_product_sales['customer_id'],  # Menambahkan label di setiap bar
+        textposition='outside',  # Menampilkan label di luar bar
+        textfont=dict(
+            color='white',  # Mengatur warna teks menjadi putih
+        ),
+        marker=dict(
+            color='rgb(173, 216, 230)',  # Set color here
+        ),
+    ))
+
+    fig.update_layout(
+        title='Customer Purchase Time Over 2016 - 2018',
+        xaxis=dict(title='Product Name', tickangle=45),
+        yaxis=dict(title='Total Sales'),
+        showlegend=False,
+        width=700,
+        height=500,
+    )
+
+    st.plotly_chart(fig)
+
+    with st.expander("Interpretasi", expanded=False):
+
+            # Konten expander
+            st.write("- Waktu transaksi pelanggan paling banyak dilakukan pada siang hari yaitu sebesar 44871 transaksi.")
+            st.write("- Waktu transaksi pelanggan paling sedikit adalah pada malam hari yaitu hanya sebesar 10112 transaksi.")
+    
+    
